27d3e04ec22bb93695937a49ee10fb1a91161160
jswwWorks <jsww.works@gmail.com>
2024-01-01 20:16:33 -0500

    Finished problem 2 of Part 1

--- Part_1/numberFacts.js
+++ Part_1/numberFacts.js
@@ -18,3 +18,35 @@ async function showNumberTrivia(num) {
 
   console.log("showNumberTrivia results:", respData.text);
 }
+
+
+
+
+// Have a “race”: make a new function, showNumberRace, that asks for
+// trivia about four different numbers (using four separate requests),
+// but, as soon as one request returns, log the piece of trivia for
+// the winning number to the console.
+
+/** Request trivia about 4 different numbers, and the fastest
+ *  response is saved. Logs the trivia for the number with the
+ *  fastest response.
+ */
+async function showNumberRace(num1, num2, num3, num4) {
+
+  const promise1 = fetch(`${BASE_URL}/${num1}?json`);
+  const promise2 = fetch(`${BASE_URL}/${num2}?json`);
+  const promise3 = fetch(`${BASE_URL}/${num3}?json`);
+  const promise4 = fetch(`${BASE_URL}/${num4}?json`);
+
+  const raceWinner = await Promise.race(
+    [promise1,
+     promise2,
+     promise3,
+     promise4
+    ]
+  )
+
+  const trivia = await raceWinner.json();
+
+  console.log(trivia.text);
+}
