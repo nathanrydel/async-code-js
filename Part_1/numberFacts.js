92bf436ad0441289201a1592ddf516755376a3ef
Nathan Rydel <nathanrydel@gmail.com>
2024-01-01 18:00:22 -0700

    updated location of global variable

--- Part_1/numberFacts.js
+++ Part_1/numberFacts.js
@@ -1,9 +1,9 @@
 "use strict";
 
+const BASE_URL = "http://numbersapi.com";
+
 // Write a function, showNumberTrivia, that makes a request to the Numbers API (http://numbersapi.com) to get trivia about your favorite number. (Make sure you get back JSON — you may need to look at the documentation of the API to see how to do this: Details.) Log the piece of trivia to the console.
 
-
-const BASE_URL = "http://numbersapi.com";
 
 /**
  * Request trivia about your favorite number and log the fact
